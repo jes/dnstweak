7b69f87596225776a3db5b322289309c78c5a9c4
James Stanley <james@incoherency.co.uk>
2023-07-11 12:58:06 +0100

    tidy

--- main.go
+++ main.go
@@ -9,6 +9,32 @@ import (
 	"strings"
 )
 
+// a spec should be like foo.example.com=1.2.3.4,5.6.7.8
+func parseOverrideSpec(spec string, override map[string][]net.IP) {
+	host, ips_csv, found := strings.Cut(spec, "=")
+	if !found {
+		log.Fatalf("spec '%s' does not contain '='\n", spec)
+	}
+
+	// append trailing dot for FQDN
+	if host[len(host)-1] != '.' {
+		host = host + "."
+	}
+	if _, exists := override[host]; !exists {
+		override[host] = make([]net.IP, 0)
+	}
+
+	// add each of the IP addresses
+	ips := strings.Split(ips_csv, ",")
+	for _, ipstr := range ips {
+		ip := net.ParseIP(ipstr)
+		if ip == nil {
+			log.Fatalf("can't parse ip address '%s'", ipstr)
+		}
+		override[host] = append(override[host], ip)
+	}
+}
+
 func main() {
 	listen := flag.String("listen", "127.0.0.1:1053", "listen address (IP:PORT or just PORT)")
 	upstream := flag.String("upstream", "8.8.8.8:53", "upstream DNS server (IP:PORT or just IP)")
@@ -21,34 +47,6 @@ func main() {
 	}
 	flag.Parse()
 
-	override := make(map[string][]net.IP)
-
-	// a spec should be like foo.example.com=1.2.3.4,5.6.7.8
-	for _, spec := range flag.Args() {
-		host, ips_csv, found := strings.Cut(spec, "=")
-		if !found {
-			log.Fatalf("spec '%s' does not contain '='\n", spec)
-		}
-
-		// append trailing dot for FQDN
-		if host[len(host)-1] != '.' {
-			host = host + "."
-		}
-		if _, exists := override[host]; !exists {
-			override[host] = make([]net.IP, 0)
-		}
-
-		// add each of the IP addresses
-		ips := strings.Split(ips_csv, ",")
-		for _, ipstr := range ips {
-			ip := net.ParseIP(ipstr)
-			if ip == nil {
-				log.Fatalf("can't parse ip address '%s'", ipstr)
-			}
-			override[host] = append(override[host], ip)
-		}
-	}
-
 	listenAddress := *listen
 	if !strings.Contains(listenAddress, ":") {
 		listenAddress = "127.0.0.1:" + listenAddress
@@ -57,6 +55,11 @@ func main() {
 	upstreamAddress := *upstream
 	if !strings.Contains(upstreamAddress, ":") {
 		upstreamAddress = upstreamAddress + ":53"
+	}
+
+	override := make(map[string][]net.IP)
+	for _, spec := range flag.Args() {
+		parseOverrideSpec(spec, override)
 	}
 
 	dnstweak := DnsTweak{
