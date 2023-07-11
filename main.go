5edf1cca0336522c44a399f9ea6145df3765c167
James Stanley <james@incoherency.co.uk>
2023-07-11 10:54:39 +0100

    Working resolver

--- main.go
+++ main.go
@@ -8,24 +8,32 @@ import (
 )
 
 type MyHandler struct {
+	// TODO: should contain map of answer replacements
 }
 
 func (handler *MyHandler) ServeDNS(w dns.ResponseWriter, msg *dns.Msg) {
-	response := new(dns.Msg)
-	response.SetReply(msg)
+	c := new(dns.Client)
+	c.Net = "udp"
 
+	r, rtt, err := c.Exchange(msg, "8.8.8.8:53") // TODO: not hard-coded
+	if err != nil {
+		// TODO: not fatal
+		log.Fatal(err)
+	}
+
+	fmt.Println("rtt = %v", rtt)
 	for _, question := range msg.Question {
 		fmt.Printf("question: %v\n", question)
 	}
 
-	w.WriteMsg(response)
+	w.WriteMsg(r)
 }
 
 func main() {
 	handler := MyHandler{}
 
 	server := dns.Server{
-		Addr:    "127.0.0.1:1053",
+		Addr:    "127.0.0.1:1053", // TODO: not hard-coded port
 		Net:     "udp",
 		Handler: &handler,
 	}
